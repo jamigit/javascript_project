f1c2fbecf244b976bb16ade6b60916d7580f23a1
jamigit <jamiehbarter@gmail.com>
2017-02-20 20:51:26 +1300

    Done!

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 
 $(document).ready(function(){
-
-  boxGen();
-
+  boxGen(genSize);
+  newGame();
 });
 
 
@@ -12,17 +11,22 @@ $(document).ready(function(){
 
 
 
-var height = parseInt(prompt("How many boxes high?"), 10);
-var width = parseInt(prompt("How many boxes wide?"), 10);
+var genSize = parseInt(prompt("How many boxes should the game be?"), 10);
+//var size = parseInt(prompt("How many boxes should the game be?"), 10);
 
 
 
-var boxGen = function(){
 
-for(i=0; i<height; i++)
+var boxGen = function(theSize){
+
+var size = theSize;
+for(i=0; i<size; i++)
   $("#theBox").append('<tr class="newRow"></tr>');
-    for(j=0; j<width; j++){
+
+    for(j=0; j<size; j++){
       $(".newRow").append('<td class="newData"></td>');
+      $(".newData").width(600/size);
+      $(".newData").height(600/size);
     }
 hoverEffect();
 };
@@ -34,4 +38,17 @@ $('.newData').hover(
     $(this).addClass('change');
 })
 
+};
+
+
+
+function newGame(){
+
+  $('#reset').click(
+    function(){
+      $("#theBox").empty();
+      var newSize = parseInt(prompt("How big should the new game be?"),10);
+
+         boxGen(newSize);
+      })
 };
